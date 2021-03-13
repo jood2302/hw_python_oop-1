0ba8f049c1d33a4884994b8884e86ac5a71c5a1c
Ковылин Василий <coherentus@gmail.com>
2021-03-13 15:53:32 +0300

    second edition

--- homework.py
+++ homework.py
@@ -1,161 +1,124 @@
-# Project: Sprint 2. Calculator calories/money (four classes). Yandex.Praktikum.Python developer.
-# Author: Vasiliy Kovylin. Backend, Cohort 16, Student.
-#
-# Description:
-# Need make four classes for two calculators: 
-# Class Record
-# Class Calculator
-# Class CaloriesCalculator
-# Class CashCalculator
-#
-# Methods for Record:                 # first brick in the wall...
-# no methods, only data
+"""Project: Sprint 2. Calculator calories/money (four classes).
 
-#
-# Methods for Calculator:             # Parent for CaloriesCalculator and CashCalculator, or for another simplest calculators
-# add_record()
-# get_today_stats()
-# get_week_stats()
-#
-# Methods for CaloriesCalculator:     # Child of Calculator wiht own new method get_calories_remained()
-# get_calories_remained()
-#
-# Methods for CashCalculator:         # Child of Calculator wiht own new method get_today_cash_remained(currency)
-# get_today_cash_remained(currency)    
-
-
+Class Record
+Class Calculator
+Class CaloriesCalculator
+Class CashCalculator.
+"""
 
 import datetime as dt
-from typing import List
+from typing import List, Optional
 
 LOCAL_DATE_FORMAT: str = '%d.%m.%Y'
 
 
-def time_to_date(dt_time: dt.datetime) -> dt.date:
-    """ convert datetime in date only
-
-    """
-    return dt.date(dt_time.year, dt_time.month, dt_time.day)
-
-
 class Record:
-    """ is object of 3 values: 'amount', 'comment' and 'date'
+    """Object of 3 values: 'amount', 'comment' and 'date'.'"""
 
-    """
+    def __init__(self, amount: float, comment: str,
+                 date: Optional[str] = None) -> None:
+        """if value 'date' skiiped, turn today."""
 
-    def __init__ (self, amount: float, comment: str, date: str = None) -> None: 
-        """ Needs 3 named values: 'amount', 'comment' and 'date'
-            In call of this, value 'date' may be skipped
-            In this case, in the value 'date' should be written the value of current day.
-
-        """
         self.amount: float = amount
         self.comment: str = comment
-        
+
         if date is None:
             self.date: dt.date = dt.date.today()
         else:
-            self.date: dt.date = time_to_date(dt.datetime.strptime(date, LOCAL_DATE_FORMAT))
+            self.date: dt.date = (dt.datetime.date(dt.datetime.strptime(date,
+                                  LOCAL_DATE_FORMAT)))
 
 
 class Calculator:
-    """ Parent class. Contains basic functionality, including list or records.
+    """Parent class. Contains basic functionality,
+       including list or records."""
 
-    """
     def __init__(self, limit: int) -> None:
-        """ Value "limit" - day limit for proper functioning own methods
-
-        """
+        """Value "limit" - day limit for proper functioning own methods."""
         self.limit: int = limit
-        self.records: List[Record] = [] 
+        self.records: List[Record] = []
 
     def add_record(self, record: Record) -> None:
-        """  Adds record to self list of records. Incoming - obj of class Record 
-
-        """
         self.records.append(record)
 
     def get_today_stats(self) -> float:
-        """ calculates sum of field 'amount' in self list 'records' for current date
+        """calculates sum of field 'amount' in self list 'records'
+        for current date.
+        """
+        return self.get_day_stats(dt.date.today())
 
-        """
-        today: dt.date = dt.date.today()
-        today_sum: float = 0
-        for rec in self.records:
-            if rec.date == today:
-                today_sum += rec.amount
-        return today_sum
-        
     def get_week_stats(self) -> float:
-        """ calculates sum of field 'amount' in self list 'records' for current date
-            and six days ago
+        """calculates sum of field 'amount' in self list 'records' for
+        current date and six days ago.
         """
-        today: dt.date = dt.date.today()
-        week_sum: float = 0
-        for rec in self.records:
-            if rec.date > today - dt.timedelta(days=7) and rec.date <= today:
-                week_sum += rec.amount
-        return week_sum        
+
+        today_date: dt.date = dt.date.today()
+        tmp_list: List = [x.amount for x in self.records
+                          if (dt.timedelta(days=7)
+                              > (today_date - x.date)
+                              >= dt.timedelta(days=0))]
+
+        return sum(v for v in tmp_list)
+
+    def today_balance(self) -> float:
+        return self.limit - self.get_today_stats()
+
+    def get_day_stats(self, date: dt.date) -> float:
+        tmp_list: List = [x.amount for x in self.records if x.date == date]
+        return sum(v for v in tmp_list)
+
 
 class CaloriesCalculator(Calculator):
-    """ Child class of 'Calculator'. All of this included.
-        Added new own method
-
+    """Child class of 'Calculator'. All of this included.
+    Added new own method get_calories_remained().
     """
-    def get_calories_remained(self) -> str:
-        """ Calculates difference between today_stats and day_limit
-            and print first or second message for user
 
+    def get_calories_remained(self) -> str:
+        """Calculates difference between today_stats and day_limit
+        and print first or second message for user.
         """
-        is_over: float = self.limit - self.get_today_stats()
-        if is_over > 0:
-            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {is_over} кКал'
-        else:
-            return f'Хватит есть!'
+
+        overhead: float = self.today_balance()
+        if overhead > 0:
+            return ('Сегодня можно съесть что-нибудь ещё,'
+                    f' но с общей калорийностью не более {overhead} кКал')
+        return 'Хватит есть!'
+
 
 class CashCalculator(Calculator):
-    """ Child class of 'Calculator'. All of this included.
-        Added new own method
-
+    """Simple upgrade of class Calculator for specific
+    functionality.
     """
+
     EURO_RATE: float = 87.94
     USD_RATE: float = 73.91
+    RUB_RATE: float = 1.0
 
-    def __init__(self, limit: int) -> None:
-        """ Currency rate are defined into class
-
+    def get_today_cash_remained(self, currency: str) -> str:
+        """Get currency name, calculate today balance,
+        return message of result in carrency's dimensions.
         """
-        super().__init__(limit)
 
-        #self.EURO_RATE: float = 87.94
-        #self.USD_RATE: float = 73.91
+        self.currency_attrib: dict = {'rub': 'руб',
+                                      'eur': 'Euro',
+                                      'usd': 'USD'}
 
-    def get_today_cash_remained(self, currency: str) -> str :  # принимает три значения "rub", "usd" или "eur"
-        """ From call wait 1 of 3 currency name, calculates balance and 
-            return message with the value in the recalculated form.        
+        self.currency_rate: dict = {'rub': self.RUB_RATE,
+                                    'eur': self.EURO_RATE,
+                                    'usd': self.USD_RATE}
 
-        """
-        
         now_cash: float = self.limit - self.get_today_stats()
 
-        if currency == 'usd':
-            multiplier: float = 1 / self.USD_RATE
-            currency_name: str = 'USD'
-        elif currency == 'eur':
-            multiplier: float = 1 / self.EURO_RATE
-            currency_name: str = 'Euro'
-        else:
-            multiplier: float = 1
-            currency_name: str = 'руб'
-
         if now_cash == 0:
-            return f'Денег нет, держись'
+            return 'Денег нет, держись'
 
-        elif now_cash < 0:
-            now_cash *= -1
-            return f'Денег нет, держись: твой долг - {round(now_cash *  multiplier, 2)} {currency_name}'
-        else:
-            return f'На сегодня осталось {round(now_cash *  multiplier, 2)} {currency_name}'
-        
-        
-        
+        divider: float = self.currency_rate[currency]
+        now_cash_currency: float = round(now_cash / divider, 2)
+        currency_name: str = self.currency_attrib[currency]
+
+        if now_cash_currency < 0:
+            now_cash_currency = abs(now_cash_currency)
+            return ('Денег нет, держись: твой долг - '
+                    f'{now_cash_currency} {currency_name}')
+        return ('На сегодня осталось '
+                f'{now_cash_currency} {currency_name}')
